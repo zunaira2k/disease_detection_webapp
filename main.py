04fc3df7c5e2745d130477af9608e4178b57ee73
sagar aggarwal <sagar_aggarwal@outlook.com>
2021-12-09 06:44:47 +0530

    changes by sagar

--- main.py
+++ main.py
@@ -7,9 +7,33 @@ import pickle
 file = open('model.pkl','rb')
 clf = pickle.load(file)
 file.close()
-
-@app.route('/', methods=["GET", "POST"])
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/corona.html', methods=["GET", "POST"])
 def hello_world():
+    # if request.method == "POST":
+    #     myDict = request.form
+    #     fever = int(myDict['fever'])
+    #     age = int(myDict['age'])
+    #     bodyPain = int(myDict['bodyPain'])
+    #     runnyNose = int(myDict['runnyNose'])
+    #     tired = int(myDict['tired'])
+    #     diarreha = int(myDict['diarreha'])
+    #     sore_throat = int(myDict['sore_throat'])
+    #     cough = int(myDict['cough'])
+    #     diffBreath = int(myDict['diffBreath'])
+    
+
+    #     # Code for inference
+    #     inputFeatures = [fever, bodyPain, age, runnyNose, diffBreath, tired, diarreha, sore_throat, cough ]
+    #     infProb =clf.predict_proba([inputFeatures])[0][1]
+    #     print(infProb)
+    #     return render_template('show.html', inf=round(infProb*100))
+    return render_template('corona.html')
+    # return 'Hello, World!' + str(infProb)
+@app.route('/show.html',methods=["GET","POST"])
+def show():
     if request.method == "POST":
         myDict = request.form
         fever = int(myDict['fever'])
@@ -28,8 +52,6 @@ def hello_world():
         infProb =clf.predict_proba([inputFeatures])[0][1]
         print(infProb)
         return render_template('show.html', inf=round(infProb*100))
-    return render_template('index.html')
-    # return 'Hello, World!' + str(infProb)
 
 
 if __name__ == "__main__":
